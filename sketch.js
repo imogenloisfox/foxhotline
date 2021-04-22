39f2e6f211e8e39f04e3dff7fd766904cc614b2e
Imogen Fox <60386717+imogenloisfox@users.noreply.github.com>
2021-04-22 21:43:38 +0100

    changes

--- sketch.js
+++ sketch.js
@@ -1,48 +1,59 @@
-let boxSize = 300;
+//girls
+let girls = [];
+let girlNum = 30;
+
+//angle
 let angle = 0;
-let balls = [];
-let initial_z;
-let xRange;
-let colors = [];
-let ballNum = 30;
 let angleRotate = 0;
-let heart;
+let angle1 = 0;
+
+//models
 let girl;
+let flower;
+
 let seed = 100000;
+
 
 function preload(){
   girl = loadModel ('girl.obj');
+  flower = loadModel ('flower.obj');
 }
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.parent('sketch-container');
-  colorMode(HSB);
+ 
   angleMode(DEGREES);
-   //balls
-   for(let i = 0; i < ballNum; i++){
-    balls.push(new movingBall());
-  }
+
+ //girls
+ for(let i = 0; i < girlNum; i++){
+  girls.push(new movingGirls());
+}
 }
 
 function draw(){
-  background(360, 0, 100);
-  // noFill();
-  // //noStroke();
-  // stroke(255, 140, 200);
-  // angle += 0.1;
-  // rotateY(angle);
-  // box(boxSize);
-  
+  background(0);
   randomSeed(seed);
-  for(let i = 0; i < ballNum; i++){
-    balls[i].move();
-    balls[i].display();
+
+  let locX = mouseX - height / 2;
+  let locY = mouseY - width / 2;
+
+  ambientLight(181, 255, 208);
+  directionalLight(10,10,10, 0.15, 0.15, 0);
+  pointLight(255, 255, 255, locX, locY, 100);
+  
+  flowerBG();
+  flowerBG2();
+  flowerBG3();
+  
+  for(let i = 0; i < girlNum; i++){
+    girls[i].move();
+    girls[i].display();
   }
   
 }
 
-class movingBall{
+class movingGirls{
   constructor(){
     this.x = random(width);
     this.y = random(height);
@@ -52,7 +63,7 @@ class movingBall{
     this.valy = random(200);
     this.valz = random(100);
     this.size = random(0.05, 0.5);
-   this.c = color(random(360), random(100), random(100));
+    this.c = color(random(100,181), random(150,255), random(100,208));
   }
   
   move(){
@@ -75,11 +86,58 @@ class movingBall{
     //rotateZ(angleRotate);
     noFill();
     stroke(this.c);
-    strokeWeight(random(0.05, 0.5));
+    strokeWeight(random(0.05, 0.4));
     scale(this.size);
     model(girl);
     pop();
   }
+}
+
+
+
+function flowerBG(){
+  push();
+  translate(250, height*0.3, 0);
+  angle1 += 0.2;
+  rotateX(160);
+  rotateY(angle1);
+  scale(0.42);
+  noStroke();
+  //stroke(0);
+  specularMaterial(181, 255, 208);
+  model(flower);
+  pop();
+  
+}
+
+function flowerBG2(){
+  push();
+  translate(-400, height*0.35, -100);
+  angle1 += 0.2;
+  rotateX(170);
+  rotateY(-angle1);
+  scale(0.47);
+  noStroke();
+  //stroke(0);
+  specularMaterial(181, 255, 208);
+  model(flower);
+  pop();
+  
+}
+
+function flowerBG3(){
+  push();
+  translate(0, height*0.5, -200);
+  angle1 += 0.2;
+  rotateX(140);
+  rotateY(-angle1);
+  scale(0.43);
+  noStroke();
+  //stroke(0);
+  specularMaterial(181, 255, 208);
+  model(flower);
+  pop();
+  
 }
 
 

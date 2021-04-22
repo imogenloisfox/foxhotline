1af12e9c0f3ab508db84fa237be4dcf2c936574e
Imogen Fox <60386717+imogenloisfox@users.noreply.github.com>
2021-04-22 22:29:31 +0100

    jjj

--- sketch.js
+++ sketch.js
@@ -77,7 +77,7 @@ class movingGirls{
   
   display(){
     push();
-    translate(this.x - width*0.65, this.y - height*0.27, this.z - this.zRange/2);
+    translate(this.x - width*0.45, this.y - height*0.27, this.z - this.zRange/2);
     angleRotate += 0.1;
     rotateX(180);
     rotateY(angleRotate);
@@ -110,7 +110,7 @@ function flowerBG(){
 
 function flowerBG2(){
   push();
-  translate(-550, height*0.3, -100);
+  translate(-400, height*0.3, -100);
   angle1 += 0.2;
   rotateX(170);
   rotateY(-angle1);
@@ -125,7 +125,7 @@ function flowerBG2(){
 
 function flowerBG3(){
   push();
-  translate(-50, height*0.4, -200);
+  translate(50, height*0.4, -200);
   angle1 += 0.2;
   rotateX(140);
   rotateY(-angle1);
